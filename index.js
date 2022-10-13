403210edafb5408c95ee58aafa27610d70951c9b
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2022-10-13 12:09:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByTagName('button'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.innerText) {
+            case 'c': display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                }
+                catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '⌫':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default: display.innerText += e.target.innerText;
+        }
+    });
+});
